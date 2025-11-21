7cb11e5beeed68849abe95f5a51c6ebe743171b7
WildanFathir <wildanfathir612@gmail.com>
2025-11-21 22:34:37 +0700

    refactor: enhance layout structure and integrate dynamic menu data

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,37 +1,21 @@
 <template>
-  <div class="flex items-center justify-center px-6">
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
+  <div class="flex items-center justify-center h-[calc(100vh-6rem)] px-6">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full max-w-6xl">
       <CardMenu
         v-for="(menu, index) in dataMenu"
         :key="index"
         :title="menu.title"
         :icon="menu.icon"
+        :path="menu.path"
       />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const dataMenu = [
-  {
-    title: 'Antrian Online',
-    icon: 'mdi:account-group',
-  },
-  {
-    title: 'Jadwal Dokter',
-    icon: 'mdi:calendar-clock',
-  },
-  {
-    title: 'Manajemen Dokter',
-    icon: 'mdi:doctor',
-  },
-  {
-    title: 'Pengelolaan Obat',
-    icon: 'mdi:pill',
-  },
-  {
-    title: 'Pembayaran',
-    icon: 'mdi:cash-multiple',
-  },
-];
+definePageMeta({
+  layout: 'simple',
+});
+
+const { dataMenu } = useMenu();
 </script>
