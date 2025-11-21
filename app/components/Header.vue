abf1bf939bc0711ea515f584799f2c6626f842a8
WildanFathir <wildanfathir612@gmail.com>
2025-11-21 22:26:39 +0700

    feat: update Header component to use NuxtLink for login navigation

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,16 +1,17 @@
 <template>
-  <header class="w-full bg-primary px-6 py-4 shadow-md">
+  <header class="fixed top-0 left-0 right-0 w-full bg-primary px-6 py-4 shadow-md z-50">
     <nav class="flex justify-between items-center w-full">
       <div class="flex items-center gap-2">
         <img src="/images/logo.png" alt="Logo" class="size-9" />
         <h1 class="text-primary-dark font-extrabold text-lg">KLINIK VIRZA MEDIKA</h1>
       </div>
 
-      <button
+      <NuxtLink
+        to="/login"
         class="cursor-pointer py-1.5 px-2.5 bg-primary-light rounded-full shadow-md hover:shadow-lg transition-shadow"
       >
         <Icon name="line-md:account" size="28" />
-      </button>
+      </NuxtLink>
     </nav>
   </header>
 </template>
