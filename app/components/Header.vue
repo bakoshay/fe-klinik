68a8809744da5394b7c6744bbba37be8c96589ff
WildanFathir <wildanfathir612@gmail.com>
2025-11-21 17:08:23 +0700

    feat: add Header component with logo and navigation

---
+++ app/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="w-full bg-primary px-6 py-4 shadow-md">
+    <nav class="flex justify-between items-center w-full">
+      <div class="flex items-center gap-2">
+        <img src="/images/logo.png" alt="Logo" class="size-9" />
+        <h1 class="text-primary-dark font-extrabold text-lg">KLINIK VIRZA MEDIKA</h1>
+      </div>
+
+      <button
+        class="cursor-pointer py-1.5 px-2.5 bg-primary-light rounded-full shadow-md hover:shadow-lg transition-shadow"
+      >
+        <Icon name="line-md:account" size="28" />
+      </button>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts" setup></script>
