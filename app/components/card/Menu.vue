ce94633f3d7c09c75f58e66b681623c911bd4089
WildanFathir <wildanfathir612@gmail.com>
2025-11-21 22:28:45 +0700

    refactor: replace div with NuxtLink in Menu component for improved navigation

--- app/components/card/Menu.vue
+++ app/components/card/Menu.vue
@@ -1,10 +1,11 @@
 <template>
-  <div
+  <NuxtLink
+    :to="path"
     class="flex flex-col items-center justify-center bg-primary-light p-6 gap-6 rounded-xl shadow-lg cursor-pointer"
   >
     <Icon :name="icon" size="75" />
     <h1 class="font-bold text-2xl text-center">{{ title }}</h1>
-  </div>
+  </NuxtLink>
 </template>
 
 <script lang="ts" setup>
@@ -16,6 +17,10 @@ defineProps({
   icon: {
     type: String,
     default: 'mdi:account-group',
+  },
+  path: {
+    type: String,
+    default: '/',
   },
 });
 </script>
