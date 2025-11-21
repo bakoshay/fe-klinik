099677c8de54a4f24f026c7d4d3849c55a4cb58f
WildanFathir <wildanfathir612@gmail.com>
2025-11-21 17:08:05 +0700

    feat: add @nuxt/icon module to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/tailwind.css'],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  modules: ['@nuxt/icon'],
 });
