5f6043fd60c823e28bdce118f500dc020e837f24
WildanFathir <wildanfathir612@gmail.com>
2025-11-21 17:08:28 +0700

    feat: add Menu component with customizable title and icon

---
+++ app/components/card/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center bg-primary-light p-6 gap-6 rounded-xl shadow-lg cursor-pointer"
+  >
+    <Icon :name="icon" size="75" />
+    <h1 class="font-bold text-2xl text-center">{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    default: 'Menu Card',
+  },
+  icon: {
+    type: String,
+    default: 'mdi:account-group',
+  },
+});
+</script>
