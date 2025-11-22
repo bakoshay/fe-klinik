5ec090109ab77315038925d65ebce8125623d845
WildanFathir <wildanfathir612@gmail.com>
2025-11-22 10:33:06 +0700

    feat: integrate PrimeVue and PrimeUIX themes for enhanced UI components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
 import tailwindcss from '@tailwindcss/vite';
+import { definePreset } from '@primeuix/themes';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{gray.50}',
+      100: '{gray.100}',
+      200: '{gray.200}',
+      300: '{gray.300}',
+      400: '{gray.400}',
+      500: '{gray.500}',
+      600: '{gray.600}',
+      700: '{gray.700}',
+      800: '{gray.800}',
+      900: '{gray.900}',
+      950: '{gray.950}',
+    },
+    colorScheme: {
+      light: {
+        formField: {
+          hoverBorderColor: '{primary.color}',
+        },
+      },
+      dark: {
+        formField: {
+          hoverBorderColor: '{primary.color}',
+        },
+      },
+    },
+  },
+});
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -10,5 +42,15 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
 
-  modules: ['@nuxt/icon'],
+  modules: ['@nuxt/icon', '@primevue/nuxt-module'],
+  primevue: {
+    options: {
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: 'none',
+        },
+      },
+    },
+  },
 });
