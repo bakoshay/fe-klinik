658deee017e0e32ce217d4b95a3b03c5c5e121a3
WildanFathir <wildanfathir612@gmail.com>
2025-11-22 10:32:57 +0700

    feat: create custom layout with Header and Sidebar components

---
+++ app/layouts/custom.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Header />
+    <AppSidebar />
+
+    <main class="ml-64 pt-24 px-4 pb-4">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,5 @@
 <template>
-  <div>
-    <Header />
-    <Sidebar />
-
-    <main class="ml-64 pt-24 px-4 pb-4">
-      <slot />
-    </main>
-  </div>
+  <slot />
 </template>
 
 <script lang="ts" setup></script>
