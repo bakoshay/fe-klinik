0394c2bcba20cdbd9e3ea05627fa6ce174990ebc
WildanFathir <wildanfathir612@gmail.com>
2025-11-23 22:53:16 +0700

    feat: implement JadwalDokter component for displaying doctor schedules and selection

--- app/pages/jadwal-dokter.vue
+++ app/pages/jadwal-dokter.vue
@@ -1,9 +1,80 @@
 <template>
-  <span>test</span>
+  <div
+    class="flex flex-col bg-primary-light rounded-xl shadow py-6 px-14 w-full items-center gap-10"
+  >
+    <div class="flex w-full gap-4 items-start">
+      <span
+        v-for="(day, index) in days"
+        :key="index"
+        @click="onDayChange(day.value)"
+        :class="
+          daySelected === day.value
+            ? 'text-primary-light bg-primary-dark'
+            : 'bg-primary-gray hover:bg-primary-dark hover:text-primary-light cursor-pointer'
+        "
+        class="px-4 py-1.5 rounded-xl font-medium"
+        >{{ day.label }}</span
+      >
+    </div>
+
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 w-full">
+      <CardJadwalDokter
+        v-for="(docter, index) in docters"
+        :key="index"
+        :name="docter.name"
+        :specialty="docter.specialty"
+        :schedule="docter.schedule"
+        :status="docter.status"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 definePageMeta({
   layout: 'custom',
 });
+
+const daySelected = ref('senin');
+
+const onDayChange = (value: string) => {
+  daySelected.value = value;
+};
+
+const docters = ref([
+  {
+    name: 'Dr. Aulia Pratama',
+    specialty: 'Umum',
+    schedule: '08.00 - 12.00',
+    status: true,
+  },
+  {
+    name: 'Dr. Siti Aminah',
+    specialty: 'Gigi',
+    schedule: '10.00 - 14.00',
+    status: false,
+  },
+  {
+    name: 'Dr. Budi Santoso',
+    specialty: 'Kandungan',
+    schedule: '12.00 - 16.00',
+    status: true,
+  },
+  {
+    name: 'Dr. Rina Wijaya',
+    specialty: 'Anak',
+    schedule: '09.00 - 13.00',
+    status: true,
+  },
+]);
+
+const days = ref([
+  { label: 'Senin', value: 'senin' },
+  { label: 'Selasa', value: 'selasa' },
+  { label: 'Rabu', value: 'rabu' },
+  { label: 'Kamis', value: 'kamis' },
+  { label: "Jum'at", value: 'jumat' },
+  { label: 'Sabtu', value: 'sabtu' },
+  { label: 'Minggu', value: 'minggu' },
+]);
 </script>
