5b8507afdc30d5bca23e3e95acd2d7fc2b2927d2
WildanFathir <wildanfathir612@gmail.com>
2025-11-26 21:47:57 +0700

    feat: add useDokter composable for managing dokter API interactions

---
+++ app/composables/api/useDokter.ts
@@ -0,0 +1,20 @@
+import type {
+  DokterResponse,
+  DokterByIdResponse,
+  DokterSimpleResponse,
+  FormDokter,
+} from '@/types/dokter';
+
+export const useDokter = () => {
+  const api = useApi();
+
+  return {
+    getAll: (q?: string) => api.get<DokterResponse>(`/dokters?search=${q}`),
+    getById: (id: string) => api.get<DokterByIdResponse>(`/dokters/${id}`),
+    getByDay: (day: string) => api.get<DokterResponse>(`/dokters/hari?day=${day}`),
+    createDokter: (data: FormDokter) => api.post<DokterSimpleResponse>('/dokters', data),
+    updateDokter: (id: string, data: FormDokter) =>
+      api.put<DokterSimpleResponse>(`/dokters/${id}`, data),
+    deleteDokter: (id: string) => api.del<DokterSimpleResponse>(`/dokters/${id}`),
+  };
+};
