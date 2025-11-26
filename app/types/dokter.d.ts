b79836919f15987b135e12ff734b6f4e708e5905
WildanFathir <wildanfathir612@gmail.com>
2025-11-26 21:47:51 +0700

    feat: refactor dokter types to improve structure and add FormJadwalDokter interface

--- app/types/dokter.d.ts
+++ app/types/dokter.d.ts
@@ -1,13 +1,65 @@
-export interface Dokter {
+export interface JadwalDokter {
+  id: string;
+  dokterId: string;
+  hari: string;
+  jam_mulai: string;
+  jam_selesai: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface FormJadwalDokter {
+  day: string;
+  start: string;
+  end: string;
+}
+
+export interface FormDokter {
   name: string;
   specialty: string;
-  gender: 'L' | 'P';
+  gender: string;
   phone: string;
   address: string;
-  schedules: {
-    day: string;
-    start: string | null;
-    end: string | null;
-  }[];
   is_active: boolean;
+  schedules: FormJadwalDokter[];
+}
+
+export interface Dokter {
+  id: string;
+  nama: string;
+  spesialis: string;
+  jenis_kelamin: 'L' | 'P';
+  handphone: string;
+  alamat: string;
+  status: boolean;
+  jadwal_dokter: JadwalDokter[];
+}
+export interface DataDokter {
+  id: string;
+  nama: string;
+  spesialis: string;
+  jenis_kelamin: 'L' | 'P';
+  handphone: string;
+  alamat: string;
+  status: boolean;
+  createdAt: string;
+  updatedAt: string;
+  jadwal_dokter: JadwalDokter[];
+}
+
+export interface DokterResponse {
+  status: boolean;
+  message: string;
+  data: DataDokter[];
+}
+
+export interface DokterByIdResponse {
+  status: boolean;
+  message: string;
+  data: DataDokter;
+}
+
+export interface DokterSimpleResponse {
+  status: boolean;
+  message: string;
 }
