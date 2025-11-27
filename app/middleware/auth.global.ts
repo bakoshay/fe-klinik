eedd3a30945c4d3c1c9e02773b9d9018cece0ec1
WildanFathir <wildanfathir612@gmail.com>
2025-11-27 16:24:17 +0700

    feat: enhance authentication middleware to manage access to private pages

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,7 +2,16 @@ export default defineNuxtRouteMiddleware((to) => {
   const { getCookie } = useCookieManager();
   const token = getCookie('token');
 
+  // Jika sudah login dan ke /login, redirect ke home
   if (token && to.path === '/login') {
+    return navigateTo('/', { replace: true });
+  }
+
+  // Daftar halaman private (hanya bisa diakses jika login)
+  const privatePages = ['/manajemen-dokter', '/pengelolaan-obat', '/pembayaran'];
+
+  // Jika belum login dan akses halaman private, redirect ke /
+  if (!token && privatePages.includes(to.path)) {
     return navigateTo('/', { replace: true });
   }
 });
