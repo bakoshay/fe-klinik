0e1a18b7d639870c212fef06e91bc4031bc90d03
WildanFathir <wildanfathir612@gmail.com>
2025-11-27 22:30:27 +0700

    fix: rename useAuth function and implement logout functionality

--- app/composables/api/useAuth.ts
+++ app/composables/api/useAuth.ts
@@ -1,9 +1,17 @@
 import type { AuthLoginForm, LoginResponse } from '@/types/auth';
 
-export const useUseAuth = () => {
+export const useAuth = () => {
   const api = useApi();
+  const router = useRouter();
+  const { clearAllCookies } = useCookieManager();
+
+  const logOut = () => {
+    clearAllCookies();
+    router.push('/login');
+  };
 
   return {
     login: (data: AuthLoginForm) => api.post<LoginResponse>('/auth/login', data, false),
+    logOut,
   };
 };
