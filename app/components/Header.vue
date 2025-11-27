49808ce764b8ce2e1d3f62bbd051b94ceb968755
WildanFathir <wildanfathir612@gmail.com>
2025-11-27 16:25:57 +0700

    feat: implement logout functionality with confirmation in Header component

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -7,13 +7,57 @@
       </div>
 
       <NuxtLink
+        v-if="!token"
         to="/login"
         class="cursor-pointer py-1.5 px-2.5 bg-primary-light rounded-full shadow-md hover:shadow-lg transition-shadow"
       >
         <Icon name="line-md:account" size="28" />
       </NuxtLink>
+
+      <div
+        v-else
+        class="cursor-pointer py-1.5 px-2.5 bg-primary-light rounded-full shadow-md hover:shadow-lg transition-shadow"
+        @click="handleLogout"
+      >
+        <Icon name="line-md:logout" size="28" style="color: red" />
+      </div>
     </nav>
   </header>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { getCookie, clearAllCookies } = useCookieManager();
+
+const token = getCookie('token');
+const confirm = useConfirm();
+const toast = useToast();
+const router = useRouter();
+
+const handleLogout = async () => {
+  confirm.require({
+    message: 'Apakah Anda yakin ingin keluar?',
+    header: 'Konfirmasi Logout',
+    icon: 'pi pi-exclamation-triangle',
+    rejectLabel: 'Batal',
+    rejectProps: {
+      label: 'Batal',
+      severity: 'secondary',
+      outlined: true,
+    },
+    acceptProps: {
+      label: 'Ya, Logout',
+      severity: 'danger',
+    },
+    accept: () => {
+      clearAllCookies();
+      toast.add({
+        severity: 'success',
+        summary: 'Berhasil',
+        detail: 'Anda telah logout.',
+        life: 3000,
+      });
+      router.push({ path: '/login' });
+    },
+  });
+};
+</script>
