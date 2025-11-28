cb36ba56aacde1ee453ec5dd5bb3d4347fe818b7
WildanFathir <wildanfathir612@gmail.com>
2025-11-28 20:20:37 +0700

    fix: enhance Header component to display current queue number and next queue button

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,11 +1,28 @@
 <template>
   <header class="fixed top-0 left-0 right-0 w-full bg-primary px-6 py-4 shadow-md z-50">
     <nav class="flex justify-between items-center w-full">
+      <!-- logo -->
       <div class="flex items-center gap-2">
         <img src="/images/logo.png" alt="Logo" class="size-9" />
         <h1 class="text-primary-dark font-extrabold text-lg">KLINIK VIRZA MEDIKA</h1>
       </div>
 
+      <!-- antrian -->
+      <div v-if="token" class="flex items-center gap-1">
+        <div class="flex py-2 px-4 items-center bg-primary-light rounded-lg">
+          <p class="font-bold">{{ antrian ?? '' }}</p>
+        </div>
+
+        <button
+          @click="handleNextAntrian"
+          v-if="data?.status"
+          class="flex items-center rounded-lg cursor-pointer hover:shadow-md transition-shadow"
+        >
+          <Icon name="lucide:chevron-right" size="25" />
+        </button>
+      </div>
+
+      <!-- login -->
       <NuxtLink
         v-if="!token"
         to="/login"
@@ -14,6 +31,7 @@
         <Icon name="line-md:account" size="28" />
       </NuxtLink>
 
+      <!-- logout -->
       <div
         v-else
         class="cursor-pointer py-1.5 px-2.5 bg-primary-light rounded-full shadow-md hover:shadow-lg transition-shadow"
@@ -27,14 +45,33 @@
 
 <script lang="ts" setup>
 import { useAuth } from '@/composables/api/useAuth';
+import { useAntrian } from '@/composables/api/useAntrian';
 
 const { getCookie } = useCookieManager();
+const { logOut } = useAuth();
+const { getCurrentAntrian, nextAntrian } = useAntrian();
+const { data } = await useAsyncData('currentAntrian', () =>
+  getCurrentAntrian().then((res) => res.data.value)
+);
 
 const token = getCookie('token');
 const confirm = useConfirm();
 const toast = useToast();
 const router = useRouter();
-const { logOut } = useAuth();
+const antrian = ref<string>('');
+
+if (data.value?.status) {
+  antrian.value = data.value?.data?.nomor || '';
+} else {
+  antrian.value = 'Belum Ada Antrian';
+}
+
+const handleNextAntrian = async () => {
+  const response = await nextAntrian();
+  if (response.data.value?.status) {
+    antrian.value = response.data.value?.data?.nomor || '';
+  }
+};
 
 const handleLogout = async () => {
   confirm.require({
