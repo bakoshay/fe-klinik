d4e2d9f40280383b1aba3257d2ea6a7cf996d8fc
WildanFathir <wildanfathir612@gmail.com>
2025-11-29 15:38:00 +0700

    feat: redefine Obat interfaces for improved structure and clarity

--- app/types/obat.d.ts
+++ app/types/obat.d.ts
@@ -1,6 +1,33 @@
-export interface Obat {
-  name: string;
-  type_of_drug: 'tablet' | 'kapsul' | 'kaplet';
-  price: number;
-  is_active: boolean;
+export interface FormObat {
+  nama: string;
+  jenis: string;
+  harga: number;
+  status: boolean;
+}
+
+interface DataObat {
+  id: string;
+  nama: string;
+  jenis: 'tablet' | 'kapsul' | 'kaplet';
+  harga: number;
+  status: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ObatResponse {
+  status: boolean;
+  message: string;
+  data: DataObat[];
+}
+
+export interface ObatByIdResponse {
+  status: boolean;
+  message: string;
+  data: DataObat;
+}
+
+export interface ObatSimpleResponse {
+  status: boolean;
+  message: string;
 }
