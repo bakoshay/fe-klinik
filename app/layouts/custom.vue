14d5dbf864040a412716a0468dabc6b3c7f910e3
WildanFathir <wildanfathir612@gmail.com>
2025-11-30 20:00:06 +0700

    feat: update main layout styles for improved responsiveness

--- app/layouts/custom.vue
+++ app/layouts/custom.vue
@@ -2,7 +2,7 @@
   <Header />
   <AppSidebar />
 
-  <main class="ml-64 pt-24 px-4 pb-4">
+  <main class="md:ml-36 md:pt-[87px] lg:ml-64 lg:pt-24 pt-16 px-4 pb-16 md:pb-4">
     <slot />
   </main>
 </template>
