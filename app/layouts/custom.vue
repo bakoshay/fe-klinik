75faf1b40683825460b46f1aeff7f9dbbd17cfe2
WildanFathir <wildanfathir612@gmail.com>
2025-12-01 14:19:47 +0700

    feat: adjust padding in main layout for improved spacing

--- app/layouts/custom.vue
+++ app/layouts/custom.vue
@@ -2,7 +2,7 @@
   <Header />
   <AppSidebar />
 
-  <main class="md:ml-36 md:pt-[87px] lg:ml-64 lg:pt-24 pt-16 px-4 pb-16 md:pb-4">
+  <main class="md:ml-36 md:pt-[87px] lg:ml-64 lg:pt-24 pt-20 px-4 pb-16 md:pb-4">
     <slot />
   </main>
 </template>
