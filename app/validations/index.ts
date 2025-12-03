21dadccdcfdf5855714a8923fa209fda84d3604d
WildanFathir <wildanfathir612@gmail.com>
2025-12-03 15:04:40 +0700

    feat: add validation exports for antrian, login, dokter, obat, and pembayaran forms

---
+++ app/validations/index.ts
@@ -0,0 +1,5 @@
+export { antrianValidation } from '@/validations/antrianValidation';
+export { loginValidation } from '@/validations/loginValidation';
+export { dokterValidation } from '@/validations/dokterValidation';
+export { obatValidation } from '@/validations/obatValidation';
+export { pembayaranValidation } from '@/validations/pembayaranValidation';
