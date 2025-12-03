fc371c7f43e8c26539ef7ea7065756b2abd1f7fa
WildanFathir <wildanfathir612@gmail.com>
2025-12-03 15:29:43 +0700

    fix: enhance Menu card responsiveness by adding full width and height classes

--- app/components/card/Menu.vue
+++ app/components/card/Menu.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink
     :to="path"
-    class="flex flex-col items-center justify-center bg-primary-light p-6 gap-6 rounded-xl shadow-lg cursor-pointer"
+    class="flex flex-col items-center justify-center bg-primary-light p-6 gap-6 rounded-xl shadow-lg cursor-pointer w-full h-full"
   >
     <Icon :name="icon" size="75" />
     <h1 class="font-bold text-2xl text-center">{{ title }}</h1>
